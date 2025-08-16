8a5df1dabf88be359ac8cc1529c2dfe812527d15
taiki-yamada <yamada_taiki@cyberagent.co.jp>
2025-08-16 23:23:34 +0900

    mod

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,13 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: '/child-todo/',
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: './',
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: { alias: { '@': path.resolve(__dirname, '.') } },
+  };
 });
